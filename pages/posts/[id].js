403b4156ec3e1e4f9bfb51b25df27beeb46a6e79
Jacob Marshall <29145479+jacobhq@users.noreply.github.com>
2021-11-03 15:29:47 +0000

    🚨 Fix unused imports

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,6 @@
-import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
-import utilStyles from '../../styles/utils.module.css'
 import React, {Component} from "react";
 
 export class Comments extends Component {
