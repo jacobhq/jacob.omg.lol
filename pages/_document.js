bc1c295a14eb6025b9aed238a89c5beb03aa6a6f
jacobhq <jacob.marshall.jem@gmail.com>
2021-11-04 14:35:49 +0000

    🚀 Fetch manifest

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,9 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel='manifest' href='/manifest.json' />
+        </Head>
         <body>
           {/* 👇 Here's the script */}
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
