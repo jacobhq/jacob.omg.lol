89e38e97bc28635a7b2e170e9761bea417e172f9
jacobhq <jacob.marshall.jem@gmail.com>
2021-11-04 13:43:27 +0000

    ✨ Make keybinds clear

--- components/Menu.js
+++ components/Menu.js
@@ -11,7 +11,10 @@ import {
     Input,
     VStack,
     ButtonGroup,
-    useColorMode} from '@chakra-ui/react'
+    useColorMode,
+    Kbd,
+    HStack
+} from '@chakra-ui/react'
 import { useDisclosure } from '@chakra-ui/hooks'
 import { Home, Eye, Moon, Sun, ArrowLeft, PenTool } from 'react-feather'
 import useArrowKeyNavigationHook from "react-arrow-key-navigation-hook";
@@ -24,16 +27,16 @@ const Menu = forwardRef((props, ref) => {
     useImperativeHandle(ref, () => ({
 
         openModal() {
-          onOpen()
+            onOpen()
         }
-    
-      }));
+
+    }));
     const { isOpen, onOpen, onClose } = useDisclosure()
     const { isOpen: isThemesOpen, onOpen: onThemesOpen, onClose: onThemesClose } = useDisclosure()
     const parentRef = useArrowKeyNavigationHook({ selectors: "[data-cmd]" });
     const themeRef = useArrowKeyNavigationHook({ selectors: "[data-cmd]" });
 
-    const itemsGlobal = [{ name: 'Writing', icon: <PenTool />, go: () => {router.push('/blog'); onClose()}}, { name: 'Themes', icon: <Eye />, go: goThemes }, { name: 'Go home', icon: <Home />, go: () => {router.push('/'); onClose()} }]
+    const itemsGlobal = [{ name: 'Writing', icon: <PenTool />, kbd: "G+B", go: () => { router.push('/blog'); onClose() } }, { name: 'Themes', icon: <Eye />, kbd: "T", go: goThemes }, { name: 'Go home', icon: <Home />, kbd: "G+H", go: () => { router.push('/'); onClose() } }]
     const itemsGlobalAttrs = ['name', 'icon']
     const [filteredItems, globalSearch, setGlobalSearch] = useSearch(itemsGlobal, itemsGlobalAttrs);
     const themes = [{ name: 'Dark', icon: <Moon />, go: toggleColorMode }, { name: 'Light', icon: <Sun />, go: toggleColorMode }, { name: 'Back', icon: <ArrowLeft />, go: goBack }]
@@ -46,7 +49,7 @@ const Menu = forwardRef((props, ref) => {
     // Site navigation
     useHotkeys('G+H', () => router.push('/'))
     useHotkeys('G+B', () => router.push('/blog'))
-    // Open theme modal
+    // Toggle theme
     useHotkeys('T', () => toggleColorMode)
 
     function goThemes() {
@@ -67,10 +70,15 @@ const Menu = forwardRef((props, ref) => {
                     <ModalBody className={search.modal} ref={parentRef}>
                         <Input className={search.input} value={globalSearch} onChange={setGlobalSearch} placeholder="Search" variant="unstyled" data-cmd />
                         <VStack align="start">
-                            {filteredItems.map((item, i) => <a key={i} className={search.item} href="#" onClick={(e) => {e.preventDefault(); item.go()}} data-cmd>
-                                <ButtonGroup>
+                            {filteredItems.map((item, i) => <a key={i} className={search.item} href="#" onClick={(e) => { e.preventDefault(); item.go() }} data-cmd>
+                                <ButtonGroup className={search.itemWrapper} width="100%">
                                     <IconButton className={search.icon, search.noFocus} icon={item.icon} onClick={item.go} variant="unstyled" tabIndex="-1" />
-                                    <Button className={search.noFocus} variant="unstyled" padding="0" isFullWidth tabIndex="-1">{item.name}</Button>
+                                    <Button className={search.noFocus} variant="unstyled" padding="0" isFullWidth tabIndex="-1" width="100%">
+                                        <HStack width="100%" justify="space-between">
+                                            <b className={search.p}>{item.name}</b>
+                                            <Kbd className={search.kbd} align="end">{item.kbd}</Kbd>
+                                        </HStack>
+                                    </Button>
                                 </ButtonGroup>
                             </a>)}
                         </VStack>
@@ -83,7 +91,7 @@ const Menu = forwardRef((props, ref) => {
                     <ModalBody className={search.modal} ref={themeRef}>
                         <Input className={search.input} value={themesSearch} onChange={setThemesSearch} placeholder="Choose a theme" variant="unstyled" data-cmd />
                         <VStack align="start">
-                            {filteredThemes.map((item, i) => <a key={i} className={search.item} href="#" onClick={(e) => {e.preventDefault(); item.go()}} data-cmd>
+                            {filteredThemes.map((item, i) => <a key={i} className={search.item} href="#" onClick={(e) => { e.preventDefault(); item.go() }} data-cmd>
                                 <ButtonGroup>
                                     <IconButton className={search.icon, search.noFocus} icon={item.icon} onClick={item.go} variant="unstyled" tabIndex="-1" />
                                     <Button className={search.noFocus} variant="unstyled" padding="0" isFullWidth tabIndex="-1">{item.name}</Button>
