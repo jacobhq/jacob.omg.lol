73ec77ae14b521e5d8744c7e8fdd3b316506bfa9
jacobhq <jacob.marshall.jem@gmail.com>
2021-11-04 19:20:22 +0000

    ✨ Make nav buttons work

--- components/Nav.js
+++ components/Nav.js
@@ -9,6 +9,7 @@ import { Home, Command } from 'react-feather'
 import { InView } from 'react-intersection-observer';
 import { useRef } from 'react';
 import Menu from './Menu';
+import router from 'next/router';
 
 export default function Nav() {
     const variant = useBreakpointValue({ base: "blur", lg: "no-blur" })
@@ -22,7 +23,7 @@ export default function Nav() {
                     <>
                         <HStack ref={ref}>
                             <Tooltip label="G H" aria-label="Press g and h to go home">
-                                <IconButton variant="ghost" size="lg" icon={<Icon as={Home} boxSize={6} />} />
+                                <IconButton variant="ghost" size="lg" icon={<Icon as={Home} boxSize={6} />} onClick={() => router.push('/')} />
                             </Tooltip>
                             <Tooltip label="CTRL K" aria-label="Press ctrl and k to open the command menu">
                                 <IconButton variant="ghost" size="lg" icon={<Icon as={Command} boxSize={6} />} onClick={() => menuRef.current.openModal()} aria-label="Open the command menu" />
@@ -30,7 +31,7 @@ export default function Nav() {
                         </HStack>
                         <HStack hidden={inView} position="fixed" left={0} top={0} width="100vw" paddingTop="15px" paddingBottom="15px" paddingLeft="15px" className={variant}>
                             <Tooltip label="G H" aria-label="Press g and h to go home">
-                                <IconButton variant="ghost" size="lg" icon={<Icon as={Home} boxSize={6} />} />
+                                <IconButton variant="ghost" size="lg" icon={<Icon as={Home} boxSize={6} />} onClick={() => router.push('/')} />
                             </Tooltip>
                             <Tooltip label="CTRL K" aria-label="Press ctrl and k to open the command menu">
                                 <IconButton variant="ghost" size="lg" icon={<Icon as={Command} boxSize={6} />} onClick={() => menuRef.current.openModal()} aria-label="Open the command menu" />
