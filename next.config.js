3b1f8aef126738c1bde58cd71ac489c8f849daeb
jacobhq <jacob.marshall.jem@gmail.com>
2021-11-04 16:39:48 +0000

    🚨 Try again

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-// Maybe this will work: https://github.com/interclip/interclip-next/commit/50785a15394345a97f1774e052cd2f32650dad1c
-const withPWA = require('next-pwa')
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
-  reactStrictMode: true,
   pwa: {
-    dest: 'public',
-    register: true,
-    runtimeCaching: true,
-    buildExcludes: [/middleware-manifest.json$/],
-  }
-})
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/]
+  },
+});
