c77908170b5ae4a021e4d73837181bf4ceded41a
Jacob Marshall <jacob@omg.lol>
2021-11-07 16:00:55 +0000

    🍱 Apple touch icons

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,15 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <Head>
+          <link rel="apple-touch-icon" sizes="57x57" href="/ios/57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/ios/60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/ios/72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="/ios/76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/ios/114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/ios/120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/ios/144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/ios/152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/ios/180.png" />
           <link rel='manifest' href='/manifest.json' />
         </Head>
         <body>
