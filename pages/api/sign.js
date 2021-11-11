2c550caa81feece8e1fda0d52e3796f6d13edfb2
Jacob Marshall <29145479+jacobhq@users.noreply.github.com>
2021-11-11 20:19:00 +0000

    🗃️ Profile photo support

--- pages/api/sign.js
+++ pages/api/sign.js
@@ -5,24 +5,47 @@ const prisma = new PrismaClient()
 
 export default async function handler(req, res) {
     const session = await getSession({ req });
+
     if (!session) {
         res.statusCode = 401
         res.end('Unauthorized')
         return
     }
 
+    const user = await prisma.user.findMany({
+        where: {
+            email: session.user.email
+        }
+    })
+
+    if (!user) {
+        await prisma.user.create({
+            data: {
+                name: session.user.name,
+                email: session.user.email,
+                image: session.user.image,
+            }
+        })
+    }    
+
     if (req.method !== 'POST') {
         return res.status(405).end()
     }
 
-    if (req.body.msg === undefined || req.body.author === undefined) {
+    if (req.body.msg === undefined) {
         return res.status(400).end()
     }
+
+    const userId = await prisma.user.findMany({
+        where: {
+            email: session.user.email
+        }
+    })
 
     const data = await prisma.message.create({
         data: {
             title: req.body.msg,
-            authorId: req.body.author.id,
+            authorId: userId[0].id,
             published: true,
         }
     })
