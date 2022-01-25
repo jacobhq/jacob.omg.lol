5c7a11e626887907848bc8ea947a64d717d75171
Jacob Marshall <jacob@omg.lol>
2022-01-25 08:40:16 +0000

    🚚 More rewrites

--- next.config.js
+++ next.config.js
@@ -3,20 +3,32 @@ const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public',
+    dest: "public",
     runtimeCaching,
-    buildExcludes: [/middleware-manifest.json$/]
+    buildExcludes: [/middleware-manifest.json$/],
   },
   async rewrites() {
     return [
-	    {
-	      source: "/bee.js",
-	      destination: "https://cdn.splitbee.io/sb.js",
-	    },
-	    {
-	      source: "/_hive/:slug",
-	      destination: "https://hive.splitbee.io/:slug",
-	    },
-	  ];
+      {
+        source: "/bee.js",
+        destination: "https://cdn.splitbee.io/sb.js",
+      },
+      {
+        source: "/_hive/:slug",
+        destination: "https://hive.splitbee.io/:slug",
+      },
+      {
+        source: "/coolsetup",
+        destination: "http://coolsetup-dev.vercel.app/",
+      },
+      {
+        source: "/matteblack",
+        destination: "http://coolsetup-dev.vercel.app/",
+      },
+      {
+        source: "/spacegrey",
+        destination: "http://coolsetup-dev.vercel.app/",
+      },
+    ];
   },
 });
