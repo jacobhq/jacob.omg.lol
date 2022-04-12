adeabe4144f85b0fcda09f4e59c61c6cc306a36a
Jacob Marshall <jacob@omg.lol>
2022-04-12 07:45:14 +0100

    🚀 It probably works now

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-    siteUrl: process.env.VERCEL_URL || 'http://localhost:3000',
+    siteUrl: process.env.VERCEL_ENV === 'production' ? 'https://jacob.omg.lol' : process.env.VERCEL_ENV === 'preview' ? process.env.VERCEL_URL : 'http://localhost:3000',
     generateRobotsTxt: true, // (optional)
 }
