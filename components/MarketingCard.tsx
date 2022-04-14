cad084dd3ac471b8c6790735fafa6a7751d121bd
Jacob Marshall <jacob@omg.lol>
2022-04-14 08:50:26 +0100

    💫 Animate marketing cards

--- components/MarketingCard.tsx
+++ components/MarketingCard.tsx
@@ -1,4 +1,5 @@
 import { Box, Heading, useColorModeValue } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 type MarketingCardTypes = {
     title: string
@@ -8,9 +9,15 @@ type MarketingCardTypes = {
 export default function MarketingCard({ title, children }: MarketingCardTypes) {
     const bg = useColorModeValue("gray.100", "gray.700")
     return (
-        <Box p={5} borderRadius={8} borderWidth="1px" mb={4} bg={bg}>
-            <Heading mb={2} w="fit-content" bgGradient='linear(to-l, #777dff, #497ce2, #37bdde, #777dff, #f154ff, #f0357c)' size="lg" bgClip='text'>{title}</Heading>
-            {children}
-        </Box>
+        <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: false, amount: 0.8 }}
+        >
+            <Box p={5} borderRadius={8} borderWidth="1px" mb={4} bg={bg}>
+                <Heading mb={2} w="fit-content" bgGradient='linear(to-l, #777dff, #497ce2, #37bdde, #777dff, #f154ff, #f0357c)' size="lg" bgClip='text'>{title}</Heading>
+                {children}
+            </Box>
+        </motion.div>
     )
 }
